a73a0ef75cd516073ddb589903c7cd3eeccf90b0
Karen <karen.bandong.2018@student.lhschools.org>
2019-05-12 20:10:33 -0400

    Declared Variables

--- src/org/lickingheights/Main.java
+++ src/org/lickingheights/Main.java
@@ -2,7 +2,14 @@ package org.lickingheights;
 
 public class Main {
 
+    static String[] computerOptions = new String[3];
+    static String computerChoice;
+    static int playerChoice;
+    static String choice;
+
+
+
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
